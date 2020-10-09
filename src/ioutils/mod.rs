816874adcfd268d84d18438ebded848e59e955d1
Vui-Chee <vuicheesiew@gmail.com>
2020-10-09 10:50:31 +0800

    Retry file creation upon failures

--- src/ioutils/mod.rs
+++ src/ioutils/mod.rs
@@ -10,6 +10,8 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use lazy_static::lazy_static;
 
+// For this use case, there is no sharing among
+// threads - so ARC not used.
 lazy_static! {
     static ref RAND: Mutex<u32> = Mutex::new(0);
 }
@@ -43,19 +45,41 @@ pub fn temp_file(dirname: &str, pattern: &str) -> Result<File> {
     if dirname.len() == 0 {
         dir = env::temp_dir();
     }
-    let mut filename = "".to_owned();
-    filename.push_str(pattern);
-    filename.push_str(&next_random());
-    dir.push(filename);
 
-    // Creates new file if it does not exist.
-    // Raises error if file already exists.
-    let tmp = OpenOptions::new().write(true).create_new(true).open(dir)?;
-    // Set read & write perms for owner of this file.
-    let metadata = tmp.metadata()?;
-    let mut permissions = metadata.permissions();
-    permissions.set_mode(0o600);
-    tmp.set_permissions(permissions)?;
+    let mut n_conflict = 0;
+    let tmp: File;
+    for _i in 1..10000 {
+        let mut filename = "".to_owned();
+        filename.push_str(dir.to_str().unwrap());
+        filename.push_str(pattern);
+        // Append random suffix to pattern
+        filename.push_str(&next_random());
 
-    Ok(tmp)
+        // Creates new file if it does not exist.
+        // Raises error if file already exists.
+        let result = OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(filename);
+
+        if result.is_err() {
+            n_conflict += 1;
+            if n_conflict > 10 {
+                let mut rand = RAND.lock().unwrap();
+                *rand = reseed();
+                // Once out of this scope, lock should be released.
+            }
+        } else {
+            tmp = result?;
+            // Set read & write perms for owner of this file.
+            let metadata = tmp.metadata()?;
+            let mut permissions = metadata.permissions();
+            permissions.set_mode(0o600);
+            tmp.set_permissions(permissions)?;
+            return Ok(tmp);
+        }
+    }
+
+    // raise IO error
+    panic!("Failed to create temporary file.")
 }
