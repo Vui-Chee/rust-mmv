ea0f522e642fc8baef892cb03a3ad9b09b59770a
Vui-Chee <vuicheesiew@gmail.com>
2020-10-12 21:42:40 +0800

    Filter empty dst upon user edit

--- src/main.rs
+++ src/main.rs
@@ -100,6 +100,7 @@ pub fn run(files: Values) -> Result<(), String> {
     let edited_lines: Vec<&str> = contents
         .trim_end_matches("\n")
         .split_terminator("\n") // Normal split can result in [""]
+        .filter(|paths| paths.len() > 0) // Clear empty paths
         .collect();
 
     // Raise error when user add/deletes a line from tmp file.
