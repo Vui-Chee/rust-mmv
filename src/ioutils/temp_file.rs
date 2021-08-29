9c69003cac2e7c1943570f8c999c43a823924567
Vui-Chee <vuicheesiew@gmail.com>
2021-08-29 21:32:20 +0800

    Replace random generator with rand crate

--- src/ioutils/temp_file.rs
+++ src/ioutils/temp_file.rs
@@ -3,51 +3,20 @@
 //! Exposes temp_file() which allows a user to
 //! create a temporary file using a custom prefix
 //! which is followed by a randomly generated
-//! 9-digit suffix.
+//! 7-9 digit suffix.
+
+use rand::prelude::*;
 
 use std::env;
 use std::fs::{create_dir, File, OpenOptions};
 use std::io::{Error, ErrorKind, Result};
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
-use std::process;
-use std::sync::Mutex;
-use std::time::{SystemTime, UNIX_EPOCH};
 
-use lazy_static::lazy_static;
-
-// For this use case, there is no sharing among
-// threads - so ARC not used.
-lazy_static! {
-    static ref RAND: Mutex<u32> = Mutex::new(0);
-}
-
-pub fn reseed() -> u32 {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_nanos();
-    let pid = process::id();
-
-    (since_the_epoch + pid as u128) as u32
-}
-
-/// Returns a random 9-digit string.
+/// Returns a random 7-9 digit string.
 pub fn next_random() -> String {
-    let mut rand = RAND.lock().unwrap();
-    if *rand == 0 {
-        *rand = reseed();
-    }
-    // Raise to u64 to avoid overflow issues
-    let r: u64 = (*rand as u64) * 1664525 + 1013904223; // constants from Numerical Recipes
-
-    // Reset rand so that each time this function is called,
-    // a new random value is generated.
-    *rand = 0;
-
-    // Lock will be unlocked when rand goes out of scope.
-    ((r as f64) % 1e9).to_string()
+    let mut rng = rand::thread_rng();
+    ((rng.gen::<f64>() * 1e18) % 1e9).to_string()
 }
 
 pub fn temp_file(dirname: &str, pattern: &str) -> Result<(File, String)> {
@@ -56,7 +25,6 @@ pub fn temp_file(dirname: &str, pattern: &str) -> Result<(File, String)> {
         dir = env::temp_dir();
     }
 
-    let mut n_conflict = 0;
     let tmp: File;
     for _i in 1..10000 {
         let mut filename = "".to_owned();
@@ -72,14 +40,7 @@ pub fn temp_file(dirname: &str, pattern: &str) -> Result<(File, String)> {
             .create_new(true)
             .open(&filename);
 
-        if result.is_err() {
-            n_conflict += 1;
-            if n_conflict > 10 {
-                let mut rand = RAND.lock().unwrap();
-                *rand = reseed();
-                // Once out of this scope, lock should be released.
-            }
-        } else {
+        if !result.is_err() {
             tmp = result?;
             // Set read & write perms for owner of this file.
             let metadata = tmp.metadata()?;
@@ -102,7 +63,6 @@ pub fn temp_dir(dirname: &str, pattern: &str) -> Result<String> {
         dir = env::temp_dir();
     }
 
-    let mut n_conflict = 0;
     for _i in 1..10000 {
         let rnd_dirname = format!("{}{}", pattern, next_random());
         let mut dirpath = dir.clone();
@@ -110,15 +70,11 @@ pub fn temp_dir(dirname: &str, pattern: &str) -> Result<String> {
         // Failed to create directory.
         if let Err(err) = create_dir(&dirpath) {
             if err.kind() == ErrorKind::AlreadyExists {
-                n_conflict += 1;
-                if n_conflict > 10 {
-                    let mut rand = RAND.lock().unwrap();
-                    *rand = reseed();
-                    // Once out of this scope, lock should be released.
-                }
+                // Continue creating a random directory.
                 continue;
             } else {
-                break; // Go to return error
+                // Some other error.
+                break;
             }
         }
 
